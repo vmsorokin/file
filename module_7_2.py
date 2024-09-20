1cd94ecc54b2c8ce7464dc2cdc8b2c1c91f6d40d
Vladimir <sorokin-v-m@yandex.ru>
2024-09-20 20:42:26 +0500

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -1,13 +1,10 @@
-import io
-from pprint import pprint
-
 
 def custom_write(file_name, strings):
     strings_positions = {}
     with open(file_name, 'w', encoding='utf-8') as file:
         for i, string in enumerate(strings):
+            strings_positions[i+1, file.tell()] = string
             file.write(string + '\n')
-            strings_positions[i, file.tell()] = string
     file.close()
     return strings_positions
 
